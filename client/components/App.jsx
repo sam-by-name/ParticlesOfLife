9c2d22888c8f8bf4a3352ac2c395274583cc13e0
sam-by-name <sam-sanders@hotmail.co.uk>
2020-02-05 23:55:19 -0500

    ParticlesOfLife boilerplate prints hello world

---
+++ client/components/App.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import {HashRouter as Router, Route} from 'react-router-dom'
+
+import Board from './Board'
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      hi: true
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <Route path='/home' component={Board} />
+      </Router>
+    )
+  }
+}
+
+export default App
