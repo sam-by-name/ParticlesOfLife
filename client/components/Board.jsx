6579a871e7d310183ffc4e8a10fce52dcc977741
sam-by-name <sam-sanders@hotmail.co.uk>
2020-02-06 01:54:46 -0500

    createBoard function

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const Board = () => {
-  return  (
-    <div className='mainTitle'>
-      <h1>Hello World</h1>
-    </div>
-  )
+class Board extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      board: [],
+      boardSize: 0
+    }
+    this.createBoard = this.createBoard.bind()
+  }
+  createBoard(x) {
+    let arr = []
+    
+    for (let i = 0; i < x; i++) {
+      let temp = []
+      for (let j = 0; j < x; j++) {
+        temp.push(0)
+      }
+      arr.push(temp)
+    }
+  }
+
+  render() {
+    return  (
+      <div className='mainTitle'>
+        <h1>Hello World</h1>
+        <button onClick={() => this.createBoard()}>click me</button>
+      </div>
+    )
+  }
 }
 
 export default Board
