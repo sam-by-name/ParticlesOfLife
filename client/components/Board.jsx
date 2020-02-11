d9cd6763b0c52f5e462a11faa8bdd37c60727e9d
sam-by-name <sam-sanders@hotmail.co.uk>
2020-02-11 12:18:25 -0500

    refactor

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -6,12 +6,12 @@ const Board = (props) => {
     <div className='board'>
       {props.board.map((row) => {
         return [
-          <div style={{backgroundColor: row[0].color, height: '1px', width: '1px'}}
+          <div style={{backgroundColor: row[0].color, height: '8px', width: '8px'}}
           className='row' key={row[0].key}
           >
             {row.map(cell => {
               return [
-                <div style={{backgroundColor: cell.color, height: '1px', width: '1px'}}
+                <div style={{backgroundColor: cell.color, height: '8px', width: '8px'}}
                 className='cell' key={cell.key}
                 >
                 </div>
