6495a10c837017cb187687c693edde9775394f85
sam-by-name <sam-sanders@hotmail.co.uk>
2020-02-29 22:13:13 -0500

    fixed Stats thead / thead tags

--- client/components/Stats.jsx
+++ client/components/Stats.jsx
@@ -6,18 +6,22 @@ const Stats = (props) => {
     <div className='stats' >
       <h2>Stats</h2>
       <table>
-        <tr>
-          <th>Gens</th>
-          <th>Born</th>
-          <th>Survived</th>
-          <th>Evolved</th>
-        </tr>
-        <tr>
-          <td>{props.gen}</td>
-          <td>{props.numBorn}</td>
-          <td>{props.numEra}</td>
-          <td>{props.numEon}</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Gens</th>
+            <th>Born</th>
+            <th>Survived</th>
+            <th>Evolved</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{props.gen}</td>
+            <td>{props.numBorn}</td>
+            <td>{props.numEra}</td>
+            <td>{props.numEon}</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
