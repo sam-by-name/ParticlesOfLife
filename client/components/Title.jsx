76919f9b8e35248fa783a78507fac2a1369f10ec
sam-by-name <sam-sanders@hotmail.co.uk>
2020-03-03 21:33:50 -0500

    swap title setInterval for requestAnimationFrame

--- client/components/Title.jsx
+++ client/components/Title.jsx
@@ -7,30 +7,28 @@ class Title extends Component {
     super()
     this.state = {
       title: arr(),
-      count: 164 // ((title[0].length * title.length) - 1) / 2
+      count: 164
     }
   }
-  componentDidMount() {
-    let timer = setInterval(() => {
-      if (this.state.count) {
-        let x = flicker(this.state.title, 2, 0, 0)
-        this.setState({
-          title: x,
-          count: this.state.count - 1
-        })
-      } else clearInterval(timer)
-    }, 3)
 
+  componentDidMount() {
+    this.draw()
+  }
+
+  draw = () => {
+    if (this.state.count) {
+      this.setState({
+        title: flicker(this.state.title),
+        count: this.state.count - 1
+      })
+      requestAnimationFrame(this.draw)
+    }
   }
 
 
   render() {
     return (
       <div className='menuTitle' style={{width: `${title[0].length * 6}px`}}>
-        {/* <span className={props.title < 2 ? 'fadeOut' : 'fadeIn'}>
-          <h1>Particles of Life</h1>
-        </span> */}
-  
         {this.state.title.map(arr => {
           return [
             <div style={{backgroundColor: color[arr], height: '6px', width: '6px'}}
